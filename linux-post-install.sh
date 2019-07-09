a7be851eb295595d2a80fb09e2aee89859eafc0a
Chris Whatley <cwhatley@mac.com>
2019-07-09 09:40:47 -0400

    Windows/linux support + docker example

---
+++ linux-post-install.sh
@@ -0,0 +1,9 @@
+@echo off
+echo Installing pygame and the carla python api.
+echo ===========
+pip install pygame
+echo ============
+echo You are now ready to run the alpha command on the
+echo python scripts in this folder.
+echo
+echo e.g., try: alpha drive manual_win
--- win-post-install.bat
+++ win-post-install.bat
@@ -5,7 +5,7 @@ pip install pygame
 echo.
 echo [1mInstalling carla python api.
 echo ============[0m
-easy_install win-api-redist\carla-0.9.5-py3.7-win-amd64.egg
+easy_install api-redist\carla-0.9.5-py3.7-win-amd64.egg
 
 echo ============[0m
 echo [1mYou are now ready to run the alpha command on the
@@ -13,4 +13,3 @@ echo python scripts in this folder.
 echo.
 echo e.g., try: alpha drive manual_win[0m
 echo.
-
